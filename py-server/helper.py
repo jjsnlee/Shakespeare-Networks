4e09ad122ed760889cb09e059596e3de93f24079
Jason Lee <jasonleebox-github@yahoo.com>
2014-02-19 05:54:21 +0000

    fix potential path issues
    git-svn-id: https://xp-dev.com/svn/ShakespeareNetwork/trunk/SKPN@5 4ed440eb-d42d-4781-8128-1afcaced31c6

--- py-server/helper.py
+++ py-server/helper.py
@@ -1,15 +1,20 @@
-import os, sys, re
+import os, re
 
 _root_dir = None
 def get_root_dir():
     global _root_dir
     if _root_dir is None:
         full_currdir = os.path.abspath(os.path.curdir)
-        path_re = re.compile('^(.+SKPN/)')
+        path_re = re.compile('^(.+)/py-server')
+        print full_currdir
         m = path_re.search(full_currdir)
         if not m:
             raise Exception('Cannot find the SKPN directory in the path.')
         _root_dir = m.group(1)
         print 'Root Directory:', _root_dir
-        #_root_dir = '/Users/jason/Projects/RenaissanceNLP'
     return _root_dir
+
+if __name__ == '__main__':
+    _root_dir = None
+    get_root_dir()
+
