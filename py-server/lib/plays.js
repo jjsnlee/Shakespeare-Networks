0d8ed20ea6beb590ebea7bc04bf81b650ef399fa
Jason Lee <jasonleebox-github@yahoo.com>
2014-05-26 19:16:35 +0000

    move ext js files one more time
    git-svn-id: https://xp-dev.com/svn/ShakespeareNetwork/trunk/SKPN@90 4ed440eb-d42d-4781-8128-1afcaced31c6

--- py-server/lib/plays.js
+++ py-server/lib/plays.js
@@ -4,53 +4,10 @@ var shPlays = angular.module('shPlays', [], function($locationProvider) {
 });
 
 shPlays.directive('myDraggable', function($document) {
-  // Example from: http://docs.angularjs.org/guide/directive
   return function(scope, element, attr) {
     addDnD($document, scope, element, attr);
   };
 });
-
-function addDnD(document, scope, element, attr) {
-	var divElmt = element[0]; // the div tag
-	// get the actual position of the image 
-
-	var popupPosn = scope.popupPosn[divElmt.id] = {
-	  startX : divElmt.offsetLeft, 
-	  startY : divElmt.offsetTop
-	};
-	popupPosn.x = popupPosn.startX;
-	popupPosn.y = popupPosn.startY;
-	
-	element.css({
-	  //position: 'relative',
-	  //border: '1px solid red',
-	  //backgroundColor: 'lightgrey',
-	  cursor: 'pointer'
-	});
- 
-	element.on('mousedown', function(event) {
-	  // Prevent default dragging of selected content
-	  event.preventDefault();
-	  popupPosn.startX = event.pageX - popupPosn.x;
-	  popupPosn.startY = event.pageY - popupPosn.y;
-	  document.on('mousemove', mousemove);
-	  document.on('mouseup', mouseup);
-	});
-
-	function mousemove(event) {
-	  popupPosn.y = event.pageY - popupPosn.startY;
-	  popupPosn.x = event.pageX - popupPosn.startX;
-	  element.css({
-	    top:  popupPosn.y + 'px',
-	    left: popupPosn.x + 'px'
-	  });
-	}
-
-	function mouseup() {
-	  document.unbind('mousemove', mousemove);
-	  document.unbind('mouseup', mouseup);
-	}
-};
 
 function createChartDirective(directiveName, toggleVar, renderChartFn) {
   shPlays.directive(directiveName, function($document) {
---
+++ py-server/static/lib/ang-dnd.js
@@ -0,0 +1,43 @@
+// Example from: http://docs.angularjs.org/guide/directive
+function addDnD(document, scope, element, attr) {
+	var divElmt = element[0]; // the div tag
+	// get the actual position of the image 
+
+	var popupPosn = scope.popupPosn[divElmt.id] = {
+	  startX : divElmt.offsetLeft, 
+	  startY : divElmt.offsetTop
+	};
+	popupPosn.x = popupPosn.startX;
+	popupPosn.y = popupPosn.startY;
+	
+	element.css({
+	  //position: 'relative',
+	  //border: '1px solid red',
+	  //backgroundColor: 'lightgrey',
+	  cursor: 'pointer'
+	});
+ 
+	element.on('mousedown', function(event) {
+	  // Prevent default dragging of selected content
+	  event.preventDefault();
+	  popupPosn.startX = event.pageX - popupPosn.x;
+	  popupPosn.startY = event.pageY - popupPosn.y;
+	  document.on('mousemove', mousemove);
+	  document.on('mouseup', mouseup);
+	});
+
+	function mousemove(event) {
+	  popupPosn.y = event.pageY - popupPosn.startY;
+	  popupPosn.x = event.pageX - popupPosn.startX;
+	  element.css({
+	    top:  popupPosn.y + 'px',
+	    left: popupPosn.x + 'px'
+	  });
+	}
+
+	function mouseup() {
+	  document.unbind('mousemove', mousemove);
+	  document.unbind('mouseup', mouseup);
+	}
+};
+
