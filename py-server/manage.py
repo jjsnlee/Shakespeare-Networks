ce501269ea7b1cde9e038ebc54eb687002859a52
Jason Lee <jasonleebox-github@yahoo.com>
2015-03-29 22:57:02 +0000

    allow for remote debugging
    git-svn-id: https://xp-dev.com/svn/ShakespeareNetwork/trunk/SKPN@194 4ed440eb-d42d-4781-8128-1afcaced31c6

--- py-server/manage.py
+++ py-server/manage.py
@@ -2,9 +2,13 @@
 import os
 import sys
 
+# This is to allow remote debugging
+# http://pydev.org/manual_adv_remote_debugger.html
+sys.path.append(r'/Users/jason/Local/share/eclipse-4.3_kepler/plugins/org.python.pydev_3.9.2.201502050007/pysrc')
+#import pydevd
+#pydevd.patch_django_autoreload(patch_remote_debugger=True, patch_show_console=True)
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfg.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfg.settings")
+  from django.core.management import execute_from_command_line
+  execute_from_command_line(sys.argv)
